b06be2abbef4a21df2f06fd1c7e0a558b4f8958a
Евгения Перова <merega89@mail.ru>
2022-11-08 00:28:32 +0300

    add java6

--- src/main/java/ru/netology/sqr/SqrtService.java
+++ src/main/java/ru/netology/sqr/SqrtService.java
@@ -1,14 +1,12 @@
 package ru.netology.sqr;
 public class SqrtService {
-    public int SqrtService(int rangeFrom, int rangeTo) {
+    public int SqrtAmountService(int rangeFrom, int rangeTo) {
         int amount = 0;
         for (int i = 10; i <= 99; i++) {
             if (i * i >= rangeFrom && i * i <= rangeTo) {
                 amount++;
-                System.out.println("Число "+ i + " попадает в диапазон [" + rangeFrom + "-" + rangeTo+"]");
             }
         }
-        System.out.println("Всего "+ amount);
         return amount;
     }
 }
--- src/test/java/ru/netology/sqr/SqrtServiceTest.java
+++ src/test/java/ru/netology/sqr/SqrtServiceTest.java
@@ -1,26 +1,19 @@
-
 package ru.netology.sqr;
 
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-
+import org.junit.jupiter.params.provider.CsvFileSource;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SQRServiceTest {
 
     @ParameterizedTest
-    @CsvSource(value = {
-            "200,300,3",
-            "100,120,1",
-            "110,121,1",
-            "0,99,0",
-            "10000,12000,0"
-    })
-    public void shouldSquareService(int rangeFrom, int rangeTo, int expected) {
+    @CsvFileSource(files="src/test/resources/list.csv")
+
+    public void SqrtAmountService(int rangeFrom, int rangeTo, int expected) {
         SqrtService service = new SqrtService();
 
-        int actual = service.squareNamberService(rangeFrom, rangeTo);
+        int count = service.SqrtAmountService(rangeFrom, rangeTo);
 
-        assertEquals(expected, actual);
+        assertEquals(expected, count);
     }
 }
