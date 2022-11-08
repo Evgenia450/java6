a2d6ded68696e88ad9e863e15f739a6228804502
Evgenia450 <113906716+Evgenia450@users.noreply.github.com>
2022-11-09 00:07:46 +0300

    Update SqrtServiceTest.java

--- src/test/java/ru/netology/sqr/SqrtServiceTest.java
+++ src/test/java/ru/netology/sqr/SqrtServiceTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class SQRServiceTest {
+public class SqrtServiceTest {
 
     @ParameterizedTest
     @CsvFileSource(files="src/test/resources/list.csv")
